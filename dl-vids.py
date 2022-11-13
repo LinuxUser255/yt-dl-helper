2c36ca5785cab464f4235bfe7824d2befdf6a9bd
Chris <atnl@protonmail.com>
2022-11-12 23:02:46 -0500

    Update dl-vids.py

--- dl-vids.py
+++ dl-vids.py
@@ -2,10 +2,18 @@
 
 import os
 
-cmd = 'yt-dlp'
-filename = 'urls.txt'
-# Execute yt-dlp on each line from urls.txt
-with open(filename, 'r') as f:
-    for line in f:
-        os.system(cmd + '   ' + line)
-        
+'''
+Create a function that will open a text file urls.txt
+and Execute yt-dlp on each line from urls.txt
+'''
+
+def open_urls_file() -> object:
+    cmd = 'yt-dlp'
+    filename = 'urls.txt'
+    # Execute yt-dlp on each line from urls.txt
+    with open(filename, 'r') as f:
+        for line in f:
+            os.system(cmd + '   ' + line)
+
+            
+open_urls_file()
